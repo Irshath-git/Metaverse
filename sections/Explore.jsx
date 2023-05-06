c8795ddfc805c893a11f7462adb10628f0b4bb0c
Irshath-git <mrirshathbscit@gmail.com>
2023-05-06 19:19:49 +0530

    new

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,34 +1,34 @@
-"use client";
-import { useState } from "react";
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { fadeIn, staggerContainer } from "../utils/motion";
-import { ExploreCard, TitleText, TypingText } from "../components";
-import { exploreWorlds } from "../constants";
+'use client';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+import { ExploreCard, TitleText, TypingText } from '../components';
+import { exploreWorlds } from '../constants';
 
 const Explore = () => {
-  const [active, setActive] = useState("world-2");
+  const [active, setActive] = useState('world-2');
   return (
-    <section className={`${styles.paddings}`} id="explore">
+    <section className={`${styles.paddings}`} id='explore'>
       <motion.div
         variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
+        initial='hidden'
+        whileInView='show'
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-        <TypingText title="| The World" textStyles="text-center" />
+        <TypingText title='| The World' textStyles='text-center' />
         <TitleText
           title={
             <>
               Choose the World you want
-              <br className="md:block hidden" />
+              <br className='md:block hidden' />
               to explore
             </>
           }
-          textStyles="text-center"
+          textStyles='text-center'
         />
-        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+        <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'>
           {exploreWorlds.map((world, index) => (
             <ExploreCard
               key={world.id}
